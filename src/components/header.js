6d5efc2cee05ebd3b2c0348d96d642578c8bdde9
John Iseghohi <rulz@Johns-MacBook-Pro.local>
2019-03-31 15:29:32 +0200

    updated images

--- src/components/header.js
+++ src/components/header.js
@@ -3,30 +3,10 @@ import PropTypes from "prop-types"
 import React from "react"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+<header className='kenburns-top'>  
+    <div className='headerContent'>
+        <div className='logo'> <img src="" /> </div>
+        <h1 className='tracking-in-expand'>Discover amazing food cooked by locals</h1>
     </div>
   </header>
 )
